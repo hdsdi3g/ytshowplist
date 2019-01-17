3c642cd38fb8614f88b58332a6c6685eb2924de3
hdsdi3g <admin@hd3g.tv>
2019-01-17 14:10:49 +0100

    Correct exec ln

--- make.bash
+++ make.bash
@@ -2,5 +2,9 @@
 
 mvn package appassembler:assemble
 
-ln -s $(realpath target/appassembler/bin/ytshowplist) .
+ln -s $(realpath target/appassembler/bin/ytshowplist) exec
+
+cp -f clientid.json target/appassembler/config/
+cp -f setup.properties target/appassembler/config/
+cp -rf credentials target/appassembler/config/
 
